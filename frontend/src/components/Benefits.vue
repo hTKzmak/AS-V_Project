f1f07ed3b489e36b8635a8ffe90eeb5c57080c96
hTKzmak <ineedrtx@gmail.com>
2024-05-05 16:22:01 +0300

    updating beneftits

--- frontend/src/components/Benefits.vue
+++ frontend/src/components/Benefits.vue
@@ -24,10 +24,34 @@ export default {
 <template>
     <h2 class="benefits-title">Наши преимущества</h2>
     <div class="benefits-list container">
-        <div class="benefits-item" v-for="info in data" :id=info.id :style="{ background: info.gradient }">
+
+        <div class="benefits-item-desktop" v-for="info in data" :id=info.id :style="{ background: info.gradient }">
             <img :src=info.img alt="benefits-image">
-            <h3>{{ info.title }}</h3>
-            <p>{{ info.description }}</p>
+            <div class="benefits-text">
+                <h3>{{ info.title }}</h3>
+                <p>{{ info.description }}</p>
+            </div>
+            <RouterLink to="/lol">подробнее</RouterLink>
+        </div>
+
+        <div class="benefits-item-adaptive" v-for="info in data" :id=info.id>
+            <div class="benefits-info">
+
+                <img id="tablet-img" :src=info.img alt="benefits-image">
+                <div class="benefits-text">
+                    <h3>{{ info.title }}</h3>
+                    <p>{{ info.description }}</p>
+                </div>
+
+                <div class="benefits-text-mobile">
+                    <div class="imageAndTitle">
+                        <img id="mobile-img" :src=info.img alt="benefits-image">
+                        <h3>{{ info.title }}</h3>
+                    </div>
+                    <p>{{ info.description }}</p>
+                </div>
+
+            </div>
             <RouterLink to="/lol">подробнее</RouterLink>
         </div>
     </div>
@@ -37,6 +61,10 @@ export default {
 .benefits-title {
     text-align: center;
     font-size: 32px;
+
+    @media screen and (max-width: 768px) {
+        font-size: 24px;
+    }
 }
 
 .benefits-list {
@@ -44,9 +72,7 @@ export default {
     justify-content: space-between;
     gap: 5px;
 
-    .benefits-item {
-        background: linear-gradient(180.04deg, #FE94A6 0.04%, #F2F2F2 65.84%);
-
+    .benefits-item-desktop {
         padding: 24px 8px 32px;
         width: 228px;
         height: 450px;
@@ -57,24 +83,26 @@ export default {
 
         border-radius: 16px;
 
-        .img {
+        img {
             width: 160px;
             height: 160px;
-            background-color: red;
             margin: 0 auto;
         }
 
-        h3 {
-            font-size: 24px;
-            margin: 16px auto 8px;
+        .benefits-text {
+            h3 {
+                font-size: 24px;
+                margin: 16px auto 8px;
+            }
+
+            p {
+                font-size: 16px;
+                color: #585656;
+                width: 215px;
+                margin: 0 auto;
+            }
         }
 
-        p {
-            font-size: 16px;
-            color: #585656;
-            width: 215px;
-            margin: 0 auto;
-        }
 
         a {
             position: absolute;
@@ -89,6 +117,105 @@ export default {
 
             border-radius: 8px;
         }
+
+        @media screen and (max-width: 1440px) {
+            display: none;
+        }
+
+    }
+
+    .benefits-item-adaptive {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .benefits-info {
+
+            display: flex;
+            align-items: center;
+
+            #tablet-img {
+                width: 64px;
+                height: 64px;
+                margin-top: -40px;
+
+                @media screen and (max-width: 768px) {
+                    display: none;
+                }
+            }
+
+            .benefits-text {
+
+                margin-left: 26px;
+                margin-right: 16px;
+
+
+                h3 {
+                    font-size: 24px;
+                    margin: 16px auto 8px;
+                }
+
+                p {
+                    font-size: 16px;
+                    color: #585656;
+                }
+
+                @media screen and (max-width: 768px) {
+                    display: none;
+                }
+            }
+
+            .benefits-text-mobile {
+                display: none;
+
+                .imageAndTitle {
+
+                    display: flex;
+                    align-items: center;
+                    gap: 8px;
+
+                    h3 {
+                        font-size: 20px;
+                        margin-top: 4px;
+                        margin-bottom: 4px;
+                    }
+
+                    #mobile-img {
+                        width: 32px;
+                        height: 32px;
+                    }
+                }
+
+                p {
+                    font-size: 16px;
+                    margin: 0;
+                    color: #585656;
+                }
+
+                @media screen and (max-width: 768px) {
+                    display: grid;
+                    gap: 8px;
+                }
+            }
+        }
+
+
+        a {
+            border: 1px solid #0071E4;
+            padding: 17px 60px;
+
+            border-radius: 8px;
+
+            @media screen and (max-width: 768px) {
+                display: none;
+            }
+        }
+    }
+
+    @media screen and (max-width: 1440px) {
+        display: grid;
+        justify-content: left;
+        row-gap: 30px;
     }
 
 }
