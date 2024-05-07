9f60ec9f4fd609718f5222d73d269688ddf1fe7c
hTKzmak <ineedrtx@gmail.com>
2024-05-07 19:32:59 +0300

    updating productitem

--- frontend/src/components/ProductItem.vue
+++ frontend/src/components/ProductItem.vue
@@ -16,7 +16,7 @@
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
-                <span class="fa fa-star"></span>
+                <span class="fa fa-star missed"></span>
                 <a href="#!">(14)</a>
             </div>
             <div class="settings">
@@ -32,26 +32,48 @@
         <h3>Apple iPhone XS Max 256 ГБ золотой</h3>
         <img class="product-image" src="../assets/img.png" alt="img">
 
-        <div class="product-info">
-            <div class="existence">
-                <div class="existence-sign"></div>
-                <p>Есть в наличии</p>
+        <div class="product-item-info">
+            <div class="product-info">
+                <div class="existence">
+                    <div class="existence-sign"></div>
+                    <p>Есть в наличии</p>
+                </div>
+                <p>Гарантия 1 год</p>
             </div>
-            <p>Гарантия 1 год</p>
+
+            <div class="price-info">
+                <h4>137 900 ₽</h4>
+                <!-- мобильная версия кнопки для покупки  -->
+                <button class="buttonElem buttonCart">137 900 ₽<img src="../assets/icons/cart.svg"></button>
+
+                <!-- мобильная версия кнопки для показа, что товар положен в корзину  -->
+                <!-- <button class="buttonElem buttonCartAdded">137 900 ₽<img src="../assets/icons/cart-added.svg"></button> -->
+                <h3>137 900 ₽</h3>
+            </div>
+
+            <!-- ПК версия кнопки для покупки  -->
+            <button class="buttonElem buttonCart"><img src="../assets/icons/cart.svg">в корзину</button>
+
+            <!-- ПК версия кнопки для показа, что товар положен в корзину  -->
+            <!-- <button class="buttonElem buttonCartAdded"><img src="../assets/icons/cart-added.svg">в корзине</button> -->
+
+            <div class="other">
+                <a href="#!">Хочу дешевле</a>
+                <a class="oneCLick" href="#!">Купить в 1 клик</a>
+            </div>
         </div>
 
-        <div class="price-info">
-            <h4>137 900 ₽</h4>
-            <button class="buttonElem buttonCart">137 900 ₽<img src="../assets/icons/cart.svg"></button>
-            <h3>137 900 ₽</h3>
+        <div class="absent">
+            <div class="product-info">
+                <div class="existence">
+                    <div class="absent-sign"></div>
+                    <p>Ожидается поступление</p>
+                </div>
+            </div>
+            <p class="text-info">Мы можем сообщить вам, когда товар появится в наличии</p>
+            <button class="buttonElem">сообщить о поступлении</button>
         </div>
 
-        <button class="buttonElem buttonCart"><img src="../assets/icons/cart.svg" alt="">в корзину</button>
-
-        <div class="other">
-            <a href="#!">Хочу дешевле</a>
-            <a class="oneCLick" href="#!">Купить в 1 клик</a>
-        </div>
     </div>
 </template>
 
@@ -77,6 +99,10 @@
 
             .checked {
                 color: #FFAD31;
+            }
+
+            .missed {
+                color: #DBDBDB;
             }
 
             a {
@@ -108,78 +134,151 @@
         height: 300px;
     }
 
-    .product-info,
-    .existence {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        font-size: 16px;
-        color: #706E6E;
+    .product-item-info {
 
-        .existence-sign {
-            width: 16px;
-            height: 16px;
-            background-color: #52D116;
-            border-radius: 100px;
-            margin-right: 2px;
-        }
-    }
+        // display: none;
 
-    .price-info {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 16px;
+        width: inherit;
 
-        h4 {
-            text-decoration: line-through;
-            font-size: 20px;
-            color: #878787;
-            margin: 0;
+        .product-info,
+        .existence {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            font-size: 16px;
+            color: #706E6E;
+
+            .existence-sign {
+                width: 16px;
+                height: 16px;
+                background-color: #52D116;
+                border-radius: 100px;
+                margin-right: 2px;
+            }
         }
 
-        h3 {
-            font-size: 32px;
-            margin: 0;
+        .price-info {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 16px;
+
+            h4 {
+                text-decoration: line-through;
+                font-size: 20px;
+                color: #878787;
+                margin: 0;
+            }
+
+            h3 {
+                font-size: 32px;
+                margin: 0;
+
+                @media screen and (max-width: 1440px) {
+                    display: none;
+                }
+            }
+
+            .buttonCart,
+            .buttonCartAdded {
+                display: none;
+
+                @media screen and (max-width: 1440px) {
+                    display: flex;
+                }
+            }
+        }
+
+        .other {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-top: 10px;
+
+            .oneCLick {
+                font-size: 16px;
+                color: #706E6E;
+            }
+
+        }
+
+        .buttonCart,
+        .buttonCartAdded {
+            padding: 20px;
+            border-radius: 8px;
 
             @media screen and (max-width: 1440px) {
                 display: none;
             }
         }
 
-        .buttonCart {
-            display: none;
-
-            @media screen and (max-width: 1440px) {
-                display: flex;
-            }
+        .buttonCartAdded {
+            background-color: #6FD91C;
         }
-    }
-
-    .other {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-top: 10px;
-
-        .oneCLick {
-            font-size: 16px;
-            color: #706E6E;
-        }
-
-    }
-
-    .buttonCart {
-        padding: 20px;
-        border-radius: 8px;
 
         @media screen and (max-width: 1440px) {
-            display: none;
+            width: 259px;
         }
+    }
+
+
+    .absent {
+        width: inherit;
+        // display: none;
+
+        .product-info,
+        .existence {
+            display: flex;
+            justify-content: left;
+            gap: 2px;
+            align-items: center;
+            font-size: 16px;
+            color: #706E6E;
+
+            .absent-sign {
+                width: 16px;
+                height: 16px;
+                background-color: #0071E4;
+                border-radius: 100px;
+                margin-right: 2px;
+            }
+
+            @media screen and (max-width: 1440px) {
+                justify-content: center;
+            }
+
+        }
+
+        .text-info {
+            margin: 0;
+            color: #878787;
+            margin-left: 18px;
+        }
+
+        button {
+            border: 1px solid #0071E4;
+            color: #0071E4;
+            background: transparent;
+
+            margin-top: 16px;
+
+            padding: 17px 10px;
+
+            font-size: 18px;
+
+            border-radius: 8px;
+
+            @media screen and (max-width: 1440px) {
+                border: none;
+                padding: 0;
+            }
+        }
+
     }
 
     @media screen and (max-width: 1440px) {
-        width: 259px;
+        text-align: center;
     }
+
 }
 </style>
