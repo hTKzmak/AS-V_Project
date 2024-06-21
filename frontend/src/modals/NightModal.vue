d1ceab717820a01750d7f72ac083602e9af7d733
hTKzmak <ineedrtx@gmail.com>
2024-06-21 20:44:05 +0300

    updated styles in NightModal.vuee and NoProduct.vue

--- frontend/src/modals/NightModal.vue
+++ frontend/src/modals/NightModal.vue
@@ -1,15 +1,14 @@
-
 <script>
 
 </script>
 
 <template>
-<div class="night_cont">
+    <div class="night_cont">
         <img src="../assets/icons/modals/amimir.png" alt="sheissleepingfr">
         <form>
             <div class="text_field">
-            <h1>Ваш заказ создан!</h1>
-            <p class="info_text">Но все менеджеры уже спят(</p>
+                <h1>Ваш заказ создан!</h1>
+                <p class="info_text">Но все менеджеры уже спят(</p>
             </div>
             <p class="info_text">Мы позвоним вам, чтобы подтвердить<br> заказ в 9 часов утра по Москве</p>
             <input value="продолжить покупки" type="submit" class="buttonElem">
@@ -18,100 +17,119 @@
 </template>
 
 <style lang="scss" scoped>
-    .night_cont{
-        display: flex;
-        @media screen and (max-width: 768px) {
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
-        
+.night_cont {
+    display: flex;
+
+    @media screen and (max-width: 768px) {
+        flex-direction: column-reverse;
+        justify-content: center;
+        align-items: center;
+        padding: 20px 0;
     }
-    img{
-        @media screen and (max-width: 768px) {
-            width: 137px;
-            height: 128px;
-        }
+
+}
+
+img {
+    @media screen and (max-width: 768px) {
+        width: 137px;
+        height: 128px;
     }
-    .social-media{
+}
+
+.social-media {
     display: flex;
     flex-direction: row;
     gap: 10px;
 }
-     .buttonElem{
-        border-radius: 8px;
-        border: 1px solid #F9F9F9;
-        background-color: transparent;
-        color: #F9F9F9;
-        font-size: 16px;
-        height: 56px;
-        width: 180px;
-        margin: 10px;
-        @media screen and (max-width: 768px) {
-            width: 100%;
-        }
-    }
-    form{
-        width: 70%;
-        display: flex;
-        flex-direction: column;
-        align-items: start;
-        justify-content: center;
-        gap: 20px;
-        @media screen and (max-width: 768px) {
-            width: 100%;
-        }
-    }
-    .inputs{
-        width: 90%;
-        align-items: row;
-        flex-wrap: wrap;
 
-    }
-    .input_field{
-        padding: 20px;
-        border: 1px solid #DBDBDB;
-        border-radius: 8px;
-        margin:5px;
-        font-size: 16px;
-    }
-    .text_field{
-        display: flex;
-        flex-direction: column;
-        align-items: start;
-        justify-content: start;
-    }
-        h1{
-            margin: 0;
-            padding: 0;
+.buttonElem {
+    border-radius: 8px;
+    border: 1px solid #F9F9F9;
+    background-color: transparent;
+    color: #F9F9F9;
+    font-size: 16px;
+    height: 56px;
+    width: 180px;
+    margin: 10px;
+    padding: 0;
+
+    @media screen and (max-width: 768px) {
         width: 100%;
-        text-align: start;
-        font-size: 40px;
-        font-weight: 700;
-        color: #BADAFF;
-        @media screen and (max-width: 768px) {
-            font-size: 20px;
-        }
+        margin: 0 0 10px 0;
     }
-    h2{
-        font-size: 24px;
-        margin: 0;
-    }
-    .info_text{
+}
+
+form {
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: center;
+    gap: 20px;
+
+    @media screen and (max-width: 768px) {
         width: 100%;
-        text-align: left;
+    }
+}
+
+.inputs {
+    width: 90%;
+    align-items: row;
+    flex-wrap: wrap;
+
+}
+
+.input_field {
+    padding: 20px;
+    border: 1px solid #DBDBDB;
+    border-radius: 8px;
+    margin: 5px;
+    font-size: 16px;
+}
+
+.text_field {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: start;
+}
+
+h1 {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    text-align: start;
+    font-size: 40px;
+    font-weight: 700;
+    color: #BADAFF;
+
+    @media screen and (max-width: 768px) {
         font-size: 20px;
-        font-weight: 500;
-        color: #BADAFF;
-        margin: 0;
-        @media screen and (max-width: 768px) {
-            font-size: 14px;
-        }
     }
-    .last-info{
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap: 20px;
+}
+
+h2 {
+    font-size: 24px;
+    margin: 0;
+}
+
+.info_text {
+    width: 100%;
+    text-align: left;
+    font-size: 20px;
+    font-weight: 500;
+    color: #BADAFF;
+    margin: 0;
+
+    @media screen and (max-width: 768px) {
+        font-size: 14px;
     }
+}
+
+.last-info {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 20px;
+}
 </style>
--- frontend/src/modals/NoProuct.vue
+++ frontend/src/modals/NoProuct.vue
@@ -1,27 +1,25 @@
-
-
 <script setup>
-    // import { ref } from 'vue';
-    // import { useModalStore } from '@/stores/ModalStore'
-    import { useCurrentProductStore } from '../stores/CurrentProductStore'
-    import ButtonElem from '@/components/UI/ButtonElem.vue';
-    // const modalStore = useModalStore()
-    const CurrentProductStore = useCurrentProductStore()
-    console.log(CurrentProductStore.image)
+// import { ref } from 'vue';
+// import { useModalStore } from '@/stores/ModalStore'
+import { useCurrentProductStore } from '../stores/CurrentProductStore'
+import ButtonElem from '@/components/UI/ButtonElem.vue';
+// const modalStore = useModalStore()
+const CurrentProductStore = useCurrentProductStore()
+console.log(CurrentProductStore.image)
 
 </script>
 
 <template>
     <div class="OneCLickContainer">
+        <p class="smallInfo" id="mobile-title">Сообщить о поступлении</p>
         <div class="mobile-credit">
-                <img class="prodImg" :src="CurrentProductStore.image" alt="">
+            <img class="prodImg" :src="CurrentProductStore.image" alt="">
 
-                <div class="mobile-info">
-                    <p class="smallInfo">Сообщить о поступлении</p>
-                    <h2>{{ CurrentProductStore.name }}</h2>
-
-                </div>
+            <div class="mobile-info">
+                <p class="smallInfo" id="desktop-title">Сообщить о поступлении</p>
+                <h2>{{ CurrentProductStore.name }}</h2>
             </div>
+        </div>
 
 
         <img class="prodImg desk-credit" :src="CurrentProductStore.image" alt="">
@@ -31,7 +29,7 @@
             <input placeholder="+7" class="input_field" type="tel">
             <!-- props: ['title', 'img', 'action', 'addedItemStyle'] -->
             <div style="width: 256px; height: 56px;">
-                <ButtonElem title="сообщить о поступлении" addedItemStyle="false"/>
+                <ButtonElem title="сообщить о поступлении" addedItemStyle="false" />
             </div>
         </div>
 
@@ -39,34 +37,45 @@
 </template>
 
 <style scoped lang="scss">
-*{
+* {
     margin: 0;
     margin-bottom: 10px;
     padding: 0;
 }
 
-.desk-credit{
+.desk-credit {
     @media screen and (max-width: 768px) {
         display: none;
     }
 }
-.mobile-credit{
+
+.mobile-credit {
     display: none;
+
     @media screen and (max-width: 768px) {
-        display:flex;
+        display: flex;
         max-width: 260px;
     }
 }
-.OneCLickContainer{
-        display: flex;
-        margin-top: 20px;
 
+.mobile-info{
+    #desktop-title{
         @media screen and (max-width: 768px) {
-        align-items: center;
+            display: none;
+        }
+    }
+}
+
+.OneCLickContainer {
+    display: flex;
+    margin-top: 20px;
+
+    @media screen and (max-width: 768px) {
         flex-direction: column;
     }
-    }
-.mobile-price{
+}
+
+.mobile-price {
     display: flex;
     align-items: center;
     gap: 20px;
@@ -74,126 +83,158 @@
 
 
 
-button{
+button {
     height: 56px;
     font-size: 16px;
+    border-radius: 8px;
 }
-img{
+
+img {
     height: 32px;
     width: 32px;
+
     @media screen and (max-width: 768px) {
         height: 24px;
         width: 24px;
     }
 }
-h3{
+
+h3 {
     font-size: 24px;
     font-weight: 700;
+
     @media screen and (max-width: 768px) {
-            font-size: 16px;
+        font-size: 16px;
     }
 }
-h2{
+
+h2 {
     font-size: 28px;
     font-weight: 700;
+
     @media screen and (max-width: 768px) {
         font-size: 16px;
     }
 }
-    .oldPrice{
-        font-size: 20px;
-        font-weight: 400;
-        color: #706E6E;
-        text-decoration:line-through;
-        @media screen and (max-width: 768px) {
+
+.oldPrice {
+    font-size: 20px;
+    font-weight: 400;
+    color: #706E6E;
+    text-decoration: line-through;
+
+    @media screen and (max-width: 768px) {
         font-size: 14px;
     }
-    }
-    .currPrice{
-        font-size: 32px;
-        font-weight: 700;
-        @media screen and (max-width: 768px) {
-        font-size: 16px;
-    }
-    }
-    .smallInfo{
-        font-size: 16px;
-        font-weight: 500;
-        color: #585656;
+}
 
-    }
-    .social-media{
-        display: flex;
-        flex-direction: row;
-        gap: 10px;
-        margin-bottom: -10px;
-    }
-    .last-info{
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap: 20px;
+.currPrice {
+    font-size: 32px;
+    font-weight: 700;
 
-    }
-    .prodImg{
-        width: 150px;
-        height: 162px;
-        margin-right: 30px;
-        @media screen and (max-width: 768px) {
-            width: 56px;
-            height: 60px;
-    }
-    }
- 
-    .buttonElem{
-        border-radius: 8px;
+    @media screen and (max-width: 768px) {
         font-size: 16px;
-        height: 56px;
-        width: 43%;
-        margin: 10px;
     }
-    form{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-    .inputs{
-        width: 90%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
+}
 
-    }
-    .input_field{
-        padding: 20px;
-        width: 209px;
-        height: 16px;
-        border: 1px solid #DBDBDB;
-        border-radius: 8px;
-        margin:5px;
-        font-size: 24px;
-    }
-    .text_field{
+.smallInfo {
+    font-size: 16px;
+    font-weight: 500;
+    color: #585656;
+
+}
+
+#mobile-title {
+    display: none;
+
+    @media screen and (max-width: 768px) {
         display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
+        text-align: start;
     }
-        h1{
-            margin: 10px;
-            padding: 0;
-        width: 100%;
-        text-align: center;
-        font-size: 32px;
-        font-weight: 700;
+}
+
+.social-media {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    margin-bottom: -10px;
+}
+
+.last-info {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 20px;
+
+}
+
+.prodImg {
+    width: 150px;
+    height: 162px;
+    margin-right: 30px;
+
+    @media screen and (max-width: 768px) {
+        width: 56px;
+        height: 60px;
     }
-    .info_text{
-        width: 80%;
-        text-align: center;
-        font-size: 16px;
-        font-weight: 500;
-        color: #585656;
-    }
+}
+
+.buttonElem {
+    border-radius: 8px;
+    font-size: 16px;
+    height: 56px;
+    width: 43%;
+    margin: 10px;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.inputs {
+    width: 90%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+
+}
+
+.input_field {
+    padding: 20px;
+    width: 209px;
+    height: 16px;
+    border: 1px solid #DBDBDB;
+    border-radius: 8px;
+    margin: 5px;
+    font-size: 24px;
+    margin: 10px 0;
+}
+
+.text_field {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+h1 {
+    margin: 10px;
+    padding: 0;
+    width: 100%;
+    text-align: center;
+    font-size: 32px;
+    font-weight: 700;
+}
+
+.info_text {
+    width: 80%;
+    text-align: center;
+    font-size: 16px;
+    font-weight: 500;
+    color: #585656;
+}
 </style>
